491e111eb365885f776d27c1312ae9cb39859f51
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-03 13:13:21 -0400

    [Presentation]: Connect ProductController with ProductService

--- src/presentation/products/controller.ts
+++ src/presentation/products/controller.ts
@@ -5,9 +5,10 @@ import { CreateProductDto } from "../../domain/dtos/products/create-product.dto"
 import { UpdateProductQuantityDto } from "../../domain/dtos/products/update-product-quantity.dto";
 import { UpdateProductPriceDto } from "../../domain/dtos/products/update-product-price.dto";
 import { UpdateProductDto } from "../../domain/dtos/products/update-product.dto";
+import { ProductService } from "../services";
 
 export class ProductController {
-  constructor() {}
+  constructor(private readonly productService: ProductService) {}
   private handleError = (error: unknown, res: Response) => {
     if (error instanceof CustomError)
       return res.status(error.statusCode).json({ error: error.message });
@@ -21,16 +22,31 @@ export class ProductController {
     const { page = 1, limit = 10 } = req.query;
     const [error, paginationDto] = PaginationDto.create(+page, +limit);
     if (error) return res.status(400).json({ error });
+
+    this.productService
+      .getAllProducts(paginationDto!)
+      .then((products) => res.json(products))
+      .catch((error) => this.handleError(error, res));
   };
 
   public getProductById = async (req: Request, res: Response): Promise<any> => {
     const id = +req.params.id;
     if (!id) return res.status(400).json({ error: "Missing id" });
+
+    this.productService
+      .getProductById(id)
+      .then((product) => res.json(product))
+      .catch((error) => this.handleError(error, res));
   };
 
   public createProduct = async (req: Request, res: Response): Promise<any> => {
     const [error, createProductDto] = CreateProductDto.create(req.body);
     if (error) return res.status(400).json({ error });
+
+    this.productService
+      .createProduct(createProductDto!)
+      .then((product) => res.json(product))
+      .catch((error) => this.handleError(error, res));
   };
 
   public updateProduct = async (req: Request, res: Response): Promise<any> => {
@@ -39,11 +55,21 @@ export class ProductController {
 
     const [error, updateProductDto] = UpdateProductDto.create(req.body);
     if (error) return res.status(400).json({ error });
+
+    this.productService
+      .updateProduct(id, updateProductDto!)
+      .then((product) => res.json(product))
+      .catch((error) => this.handleError(error, res));
   };
 
   public deleteProduct = async (req: Request, res: Response): Promise<any> => {
     const id = +req.params.id;
     if (!id) return res.status(400).json({ error: "Missing id" });
+
+    this.productService
+      .deleteProduct(id)
+      .then((product) => res.json(product))
+      .catch((error) => this.handleError(error, res));
   };
 
   public updateProductQuantity = async (
@@ -58,6 +84,11 @@ export class ProductController {
     );
 
     if (error) return res.status(400).json({ error });
+
+    this.productService
+      .updateProductQuantity(id, updateProductQuantityDto!)
+      .then((product) => res.json(product))
+      .catch((error) => this.handleError(error, res));
   };
 
   public updateProductPrice = async (
@@ -72,5 +103,10 @@ export class ProductController {
     );
 
     if (error) return res.status(400).json({ error });
+
+    this.productService
+      .updateProductPrice(id, updateProductPriceDto!)
+      .then((product) => res.json(product))
+      .catch((error) => this.handleError(error, res));
   };
 }
