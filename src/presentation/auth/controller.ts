505d858355b5bcc70afbaf009d7063bdc089c9c4
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-03 11:15:58 -0400

    [Presentation]: Connect AuthController with AuthService

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
-import { CustomError } from "../../domain/errors/custom.error";
-import { CreateUserDto } from "../../domain/dtos/users/create-user.dto";
 import { LoginUserDto } from "../../domain/dtos/auth/login-user.dto";
+import { CreateUserDto } from "../../domain/dtos/users/create-user.dto";
+import { CustomError } from "../../domain/errors/custom.error";
+import { AuthService } from "../services";
 
 export class AuthController {
-  constructor() {}
+  constructor(private readonly authService: AuthService) {}
 
   private handleError = (error: unknown, res: Response) => {
     if (error instanceof CustomError)
@@ -18,14 +19,28 @@ export class AuthController {
   public login = async (req: Request, res: Response): Promise<any> => {
     const [error, loginDto] = LoginUserDto.create(req.body);
     if (error) return res.status(400).json({ error });
+
+    this.authService
+      .login(loginDto!)
+      .then((user) => res.json(user))
+      .catch((error) => this.handleError(error, res));
   };
 
   public register = async (req: Request, res: Response): Promise<any> => {
     const [error, registerDto] = CreateUserDto.create(req.body);
     if (error) return res.status(400).json({ error });
+
+    this.authService
+      .register(registerDto!)
+      .then((user) => res.json(user))
+      .catch((error) => this.handleError(error, res));
   };
 
   public validateEmail = (req: Request, res: Response) => {
     const { token } = req.params;
+    this.authService
+      .validateEmail(token)
+      .then(() => res.json("Email was validated properly"))
+      .catch((error) => this.handleError(error, res));
   };
 }
