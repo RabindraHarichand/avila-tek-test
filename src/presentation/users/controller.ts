1823465abe567ee5642a1a5d4b6f4271b886598c
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-03 11:38:47 -0400

    [Presentation]: Connect UserController with UserService

--- src/presentation/users/controller.ts
+++ src/presentation/users/controller.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
-import { CustomError } from "../../domain/errors/custom.error";
-import { PaginationDto } from "../../shared/dtos/pagination.dto";
 import { CreateUserDto } from "../../domain/dtos/users/create-user.dto";
 import { UpdateUserDto } from "../../domain/dtos/users/update-user.dto";
+import { CustomError } from "../../domain/errors/custom.error";
+import { PaginationDto } from "../../shared/dtos/pagination.dto";
+import { UserService } from "../services";
 
 export class UserController {
-  constructor() {}
+  constructor(private readonly usersService: UserService) {}
   private handleError = (error: unknown, res: Response) => {
     if (error instanceof CustomError)
       return res.status(error.statusCode).json({ error: error.message });
@@ -19,16 +20,31 @@ export class UserController {
     const { page = 1, limit = 10 } = req.query;
     const [error, paginationDto] = PaginationDto.create(+page, +limit);
     if (error) return res.status(400).json({ error });
+
+    this.usersService
+      .getAllUsers(paginationDto!)
+      .then((user) => res.json(user))
+      .catch((error) => this.handleError(error, res));
   };
 
   public getUserById = async (req: Request, res: Response): Promise<any> => {
     const id = +req.params.id;
     if (!id) return res.status(400).json({ error: "Missing id" });
+
+    this.usersService
+      .getUserById(id)
+      .then((user) => res.json(user))
+      .catch((error) => this.handleError(error, res));
   };
 
   public createUser = async (req: Request, res: Response): Promise<any> => {
     const [error, createUserDto] = CreateUserDto.create(req.body);
     if (error) return res.status(400).json({ error });
+
+    this.usersService
+      .createUser(createUserDto!)
+      .then((user) => res.json(user))
+      .catch((error) => this.handleError(error, res));
   };
 
   public updateUser = async (req: Request, res: Response): Promise<any> => {
@@ -37,10 +53,20 @@ export class UserController {
 
     const [error, updateUserDto] = UpdateUserDto.create(req.body);
     if (error) return res.status(400).json({ error });
+
+    this.usersService
+      .updateUser(id, updateUserDto!)
+      .then((user) => res.json(user))
+      .catch((error) => this.handleError(error, res));
   };
 
   public deleteUser = async (req: Request, res: Response): Promise<any> => {
     const id = +req.params.id;
     if (!id) return res.status(400).json({ error: "Missing id" });
+
+    this.usersService
+      .deleteUser(id)
+      .then((user) => res.json(user))
+      .catch((error) => this.handleError(error, res));
   };
 }
