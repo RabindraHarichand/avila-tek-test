8d3c8abe4b721dbd1cfd9a39cc74cbf1c21b3787
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-03 11:17:46 -0400

    [Presentation]: Connect AuthRoutes with AuthController & AuthService

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,11 +1,27 @@
 import { Router } from "express";
 import { AuthController } from "./controller";
+import { envs } from "../../config/envs";
+import { UserDatasourceImpl } from "../../infrastructure/datasources/user.datasource.impl";
+import { UserRepositoryImpl } from "../../infrastructure/repositories";
+import { EmailService, AuthService } from "../services";
 
 export class AuthRoutes {
   static get routes(): Router {
     const router = Router();
 
-    const authController = new AuthController();
+    const emailService = new EmailService(
+      envs.mailer_service,
+      envs.mailer_email,
+      envs.mailer_secret_key,
+      envs.send_email
+    );
+
+    const userDatasource = new UserDatasourceImpl();
+    const userRepository = new UserRepositoryImpl(userDatasource);
+
+    const authService = new AuthService(userRepository, emailService);
+
+    const authController = new AuthController(authService);
 
     router.post("/login", authController.login);
     router.post("/register", authController.register);
