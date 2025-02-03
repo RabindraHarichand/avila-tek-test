31ba2f768c05344864d8f290bbe4034cf1a1cce1
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-03 14:38:16 -0400

    [Testing]: Add UserEntity unit test, Products routes integration test, app test & testServer

---
+++ tests/domain/entities/user.entity.unit.test.ts
@@ -0,0 +1,47 @@
+import { UserEntity } from "../../../src/domain/entities/user.entity";
+import { UserRole } from "../../../src/shared/types/user.type";
+
+describe("UserEntity", () => {
+  const dataObj = {
+    id: 1,
+    firstName: "Raul",
+    lastName: "Perez",
+    email: "test@email.com",
+    passwordHash: "123456*",
+    emailValidated: false,
+    role: "Administrator",
+  };
+  test("should create a UserEntity instance", () => {
+    const user = new UserEntity(
+      dataObj.id,
+      dataObj.firstName,
+      dataObj.lastName,
+      dataObj.email,
+      dataObj.passwordHash,
+      dataObj.emailValidated,
+      dataObj.role as UserRole
+    );
+
+    expect(user).toBeInstanceOf(UserEntity);
+    expect(user.id).toBe(dataObj.id);
+    expect(user.firstName).toBe(dataObj.firstName);
+    expect(user.lastName).toBe(dataObj.lastName);
+    expect(user.email).toBe(dataObj.email);
+    expect(user.passwordHash).toBe(dataObj.passwordHash);
+    expect(user.emailValidated).toBe(dataObj.emailValidated);
+    expect(user.role).toBe(dataObj.role);
+  });
+
+  test("should create a UserEntity instance from object", () => {
+    const user = UserEntity.fromObject(dataObj);
+
+    expect(user).toBeInstanceOf(UserEntity);
+    expect(user.id).toBe(dataObj.id);
+    expect(user.firstName).toBe(dataObj.firstName);
+    expect(user.lastName).toBe(dataObj.lastName);
+    expect(user.email).toBe(dataObj.email);
+    expect(user.passwordHash).toBe(dataObj.passwordHash);
+    expect(user.emailValidated).toBe(dataObj.emailValidated);
+    expect(user.role).toBe(dataObj.role);
+  });
+});
