c7b05f2a0d1496e310d351926608c01780e6b597
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-03 13:49:05 -0400

    [Presentation]: Connect OrderRoutes with OrderController & OrderService

--- src/presentation/orders/routes.ts
+++ src/presentation/orders/routes.ts
@@ -1,11 +1,35 @@
 import { Router } from "express";
+import { OrderDatasourceImpl } from "../../infrastructure/datasources/order.datasource.impl";
+import { ProductDatasourceImpl } from "../../infrastructure/datasources/product.datasource.impl";
+import { UserDatasourceImpl } from "../../infrastructure/datasources/user.datasource.impl";
+import {
+  OrderRepositoryImpl,
+  UserRepositoryImpl,
+  ProductRepositoryImpl,
+} from "../../infrastructure/repositories";
+import { OrderService } from "../services";
 import { OrderController } from "./controller";
 
 export class OrderRoutes {
   static get routes(): Router {
     const router = Router();
 
-    const ordersController = new OrderController();
+    const orderDatasource = new OrderDatasourceImpl();
+    const orderRepository = new OrderRepositoryImpl(orderDatasource);
+
+    const userDatasource = new UserDatasourceImpl();
+    const userRepository = new UserRepositoryImpl(userDatasource);
+
+    const productDatasource = new ProductDatasourceImpl();
+    const productRepository = new ProductRepositoryImpl(productDatasource);
+
+    const ordersService = new OrderService(
+      orderRepository,
+      userRepository,
+      productRepository
+    );
+
+    const ordersController = new OrderController(ordersService);
 
     router.get("/", ordersController.getAllOrders);
     router.get("/:id", ordersController.getOrderById);
--- src/presentation/services/index.ts
+++ src/presentation/services/index.ts
@@ -1,4 +1,5 @@
 export * from "./auth.service";
 export * from "./email.service";
+export * from "./order.service";
 export * from "./product.service";
 export * from "./user.service";
