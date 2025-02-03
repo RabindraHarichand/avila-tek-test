c4a46ba156a154b1ef8b0dc642291516c3a21c0f
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-03 10:22:27 -0400

    [Presentation]: Connect AppRoutes with UserRoutes, OrderRoutes, ProductRoutes, AuthRoutes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
+import { UserRoutes } from "./users/routes";
+import { AuthRoutes } from "./auth/routes";
+import { OrderRoutes } from "./orders/routes";
+import { ProductRoutes } from "./products/routes";
+
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
 
-    router.use("/api/users", () => {});
-    router.use("/api/orders", () => {});
-    router.use("/api/products", () => {});
-    router.use("/api/auth", () => {});
+    router.use("/api/users", UserRoutes.routes);
+    router.use("/api/orders", OrderRoutes.routes);
+    router.use("/api/products", ProductRoutes.routes);
+    router.use("/api/auth", AuthRoutes.routes);
 
     return router;
   }
