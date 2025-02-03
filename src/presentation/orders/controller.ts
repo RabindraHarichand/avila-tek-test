6c3ae822dd6b2bcb44951c9f25edfce0dc935a35
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-03 13:46:51 -0400

    [Presentation]: Connect OrderController with OrderService

--- src/presentation/orders/controller.ts
+++ src/presentation/orders/controller.ts
@@ -4,9 +4,10 @@ import { PaginationDto } from "../../shared/dtos/pagination.dto";
 import { CreateOrderDto } from "../../domain/dtos/orders/create-order-dto";
 import { UpdateOrderDto } from "../../domain/dtos/orders/update-order-dto";
 import { AuthUserDto } from "../../domain/dtos/auth/auth-user.dto";
+import { OrderService } from "../services/order.service";
 
 export class OrderController {
-  constructor() {}
+  constructor(private readonly orderService: OrderService) {}
 
   private handleError = (error: unknown, res: Response) => {
     if (error instanceof CustomError)
@@ -21,11 +22,21 @@ export class OrderController {
     const { page = 1, limit = 10 } = req.query;
     const [error, paginationDto] = PaginationDto.create(+page, +limit);
     if (error) return res.status(400).json({ error });
+
+    this.orderService
+      .getAllOrders(paginationDto!)
+      .then((orders) => res.json(orders))
+      .catch((error) => this.handleError(error, res));
   };
 
   public getOrderById = async (req: Request, res: Response): Promise<any> => {
     const id = +req.params.id;
     if (!id) return res.status(400).json({ error: "Missing id" });
+
+    this.orderService
+      .getOrderById(id)
+      .then((orders) => res.json(orders))
+      .catch((error) => this.handleError(error, res));
   };
 
   public getAllOrdersByUserId = async (
@@ -38,6 +49,11 @@ export class OrderController {
     const { page = 1, limit = 10 } = req.query;
     const [error, paginationDto] = PaginationDto.create(+page, +limit);
     if (error) return res.status(400).json({ error });
+
+    this.orderService
+      .getAllOrdersByUserId(id, paginationDto!)
+      .then((orders) => res.json(orders))
+      .catch((error) => this.handleError(error, res));
   };
 
   public createOrder = async (req: Request, res: Response): Promise<any> => {
@@ -46,11 +62,21 @@ export class OrderController {
 
     const [authError, authUserDto] = AuthUserDto.create(req.body);
     if (authError) return res.status(400).json({ error: authError });
+
+    this.orderService
+      .createOrder(createOrderDto!, authUserDto!)
+      .then((orders) => res.json(orders))
+      .catch((error) => this.handleError(error, res));
   };
 
   public deleteOrder = async (req: Request, res: Response): Promise<any> => {
     const id = +req.params.id;
     if (!id) return res.status(400).json({ error: "Missing id" });
+
+    this.orderService
+      .deleteOrder(id)
+      .then((orders) => res.json(orders))
+      .catch((error) => this.handleError(error, res));
   };
   public updateOrderStatus = async (
     req: Request,
@@ -59,5 +85,10 @@ export class OrderController {
     const id = +req.params.id;
     const [error, updateOrderDto] = UpdateOrderDto.create(req.body);
     if (error) return res.status(400).json({ error });
+
+    this.orderService
+      .updateOrderStatus(id, updateOrderDto!)
+      .then((orders) => res.json(orders))
+      .catch((error) => this.handleError(error, res));
   };
 }
