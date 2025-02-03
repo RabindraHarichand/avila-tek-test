88f9406fb5a1291c626cc69ac29a7cd349c0f90a
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-03 10:27:39 -0400

    [Presentation]: Connect AuthRoutes with AuthController

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
+import { AuthController } from "./controller";
 
 export class AuthRoutes {
   static get routes(): Router {
     const router = Router();
 
-    router.post("/login", () => {});
-    router.post("/register", () => {});
-    router.get("/validate-email/:token", () => {});
+    const authController = new AuthController();
+
+    router.post("/login", authController.login);
+    router.post("/register", authController.register);
+    router.get("/validate-email/:token", authController.validateEmail);
 
     return router;
   }
