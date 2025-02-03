23e4a5b6568ebad8fd783cf03f045a5aa0311ba7
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-03 09:08:58 -0400

    [Domain]: Definition of project entities

---
+++ src/domain/errors/entities/user.entity.ts
@@ -0,0 +1,37 @@
+import { UserRole } from "../../../shared/types/user.type";
+
+export class UserEntity {
+  constructor(
+    public id: number,
+    public firstName: string,
+    public lastName: string,
+    public email: string,
+    public passwordHash: string,
+    public emailValidated: boolean,
+    public role: UserRole
+  ) {}
+
+  public static fromObject(object: { [key: string]: any }): UserEntity {
+    const {
+      id,
+      firstName,
+      lastName,
+      email,
+      passwordHash,
+      emailValidated,
+      role,
+    } = object;
+
+    const user = new UserEntity(
+      id,
+      firstName,
+      lastName,
+      email,
+      passwordHash,
+      emailValidated,
+      role as UserRole
+    );
+
+    return user;
+  }
+}
