6e900f8c49fbd34ef6dd4fd8488d879217e003cf
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-03 07:38:14 -0400

    [config]: Add config folder with envs file to validate .env variables

---
+++ src/config/envs.ts
@@ -0,0 +1,43 @@
+import "dotenv/config";
+import * as joi from "joi";
+
+interface EnvVars {
+  PORT: number;
+  JWT_SEED: string;
+
+  SEND_EMAIL: boolean;
+  MAILER_SERVICE: string;
+  MAILER_EMAIL: string;
+  MAILER_SECRET_KEY: string;
+  WEBSERVICE_URL: string;
+}
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+    JWT_SEED: joi.string().required(),
+
+    SEND_EMAIL: joi.boolean().required(),
+    MAILER_SERVICE: joi.string().required(),
+    MAILER_EMAIL: joi.string().required(),
+    MAILER_SECRET_KEY: joi.string().required(),
+  })
+  .unknown(true);
+
+const { error, value } = envsSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVars: EnvVars = value;
+
+export const envs = {
+  port: envVars.PORT,
+  jwt_seed: envVars.JWT_SEED,
+
+  send_email: envVars.SEND_EMAIL,
+  mailer_service: envVars.MAILER_SERVICE,
+  mailer_email: envVars.MAILER_EMAIL,
+  mailer_secret_key: envVars.MAILER_SECRET_KEY,
+};
